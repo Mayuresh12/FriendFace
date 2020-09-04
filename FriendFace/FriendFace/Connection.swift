4dd99ce61267e314340573ba44c66c77b3cb1502
MRAO0212 <mayureshvrao@gmail.com>
2020-09-04 04:04:42 -0700

    fetch data

---
+++ FriendFace/FriendFace/Connection.swift
@@ -0,0 +1,15 @@
+//
+//  Connection.swift
+//  FriendFace
+//
+//  Created by Mayuresh Rao on 9/4/20.
+//  Copyright © 2020 Mayuresh Rao. All rights reserved.
+//
+
+import Foundation
+
+
+struct Connection: Codable {
+    var id: UUID
+    var name: String
+}
--- FriendFace/FriendFace/ViewController.swift
+++ FriendFace/FriendFace/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var friends = [Friend]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        DispatchQueue.global().async {
+            do {
+                let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                let downloadFriends = try decoder.decode([Friend].self, from: data)
+                DispatchQueue.main.async {
+                    self.friends = downloadFriends
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
 }
 
