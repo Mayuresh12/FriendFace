96d41893bbd3a0ef89f763a124687bf1925fc4a0
MRAO0212 <mayureshvrao@gmail.com>
2020-09-05 15:15:40 -0700

    add functionality for searching friends

---
+++ FriendFace/FriendFace/SearchFriendsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchFriendsViewController.swift
+//  FriendFace
+//
+//  Created by Mayuresh Rao on 9/5/20.
+//  Copyright © 2020 Mayuresh Rao. All rights reserved.
+//
+
+import UIKit
+
+class SearchFriendsViewController: UIViewController {
+//    func updateSearchResults(for searchController: UISearchController) {
+//        <#code#>
+//    }
+//
+//
+//    var filteredFreinds = [Friends]()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        let search = UISearchController(searchResultsController: nil)
+//        search.searchResultsUpdater = self
+//        search.obscuresBackgroundDuringPresentation = false
+//        search.searchBar.placeholder = "Find a friend"
+//        navigationItem.searchController = search
+//    }
+//
+//number
+}
